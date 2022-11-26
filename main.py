96275495117b0bd0b2a1d8431e5f583eb7aa242d
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2022-11-26 17:16:49 +0700

    '2'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
 
@@ -12,6 +12,33 @@ messages = [{'title': 'Message One',
 def index():
     return render_template('index.html', messages=messages)
 
+# ...
+
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
     return render_template('create.html')
+
+# ...
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+# ...
+
