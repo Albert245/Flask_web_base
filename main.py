d48aaa9b68fa21246dfc869a78ce88085c7af639
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2022-11-27 16:57:45 +0700

    5

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
+import os
 
+# ...
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+# ...
 
 @app.route('/')
 def index():
-    return render_template('upload.html')
-@app.route('/upload', methods=['POST'])
+    return render_template('index.html', messages=messages)
+
+# ...
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+@app.route('/upload', methods = ('GET','POST'))
 def upload():
-    file = request.files['uploadfile']
-    file.save(f'uploads/{file.filename}')
-    return redirect('/')
+    if request.method == 'POST':
+        file = request.files['uploadfile']
+        if file:
+            return redirect('/upload')
+    return render_template('upload.html')
+
+
+
