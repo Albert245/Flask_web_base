6e970a9ebd4946e944e079b80f3986c492ad579d
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2022-11-28 14:24:02 +0700

    6

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def upload():
                     Block.append(str(line.rstrip()))
                     
                 page = Datafile2hex(Block)
-                return page[0][0]
+                return page
         except:
             return 'Not allowed'
     return render_template('upload.html')
