86d19867821292cf2ea4884b4a745bc0c4654645
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2022-11-30 01:30:56 +0700

    14

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def upload():
                     Block.append(str(line.rstrip()))
                     
                 page = DP.convert_hex_file(Block)
-                base.upload(file.filename)
+                base.upload(os.path.basename(file.filename))
                 return page[0][1]
         except:
             return 'Not allowed'
