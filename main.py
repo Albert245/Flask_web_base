0ab77c9919e04f27ecc4979d58ae7ab98714dcf6
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2022-11-30 02:26:58 +0700

    15

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def upload():
                     Block.append(str(line.rstrip()))
                     
                 page = DP.convert_hex_file(Block)
-                base.upload('link.txt')
+                # base.upload('link.txt')
                 return realpath
         except:
             return 'Not allowed'
