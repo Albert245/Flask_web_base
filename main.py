5ba9c880f0d990d238dfd557cd5713fb9ad9def9
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2022-12-13 00:37:40 +0700

    23

--- main.py
+++ main.py
@@ -69,5 +69,5 @@ def upload():
                 log = AVR.AVR_ISP(TCP_IP,TCP_PORT,page)
                 return log
         except:
-            return 'Not allow'
+            return 'Not allowed'
     return render_template('upload.html')
