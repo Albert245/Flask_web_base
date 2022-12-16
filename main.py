843bb7b0e41a95aa08314d3f900b3e35d377f481
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2022-12-16 22:36:41 +0700

    25

--- main.py
+++ main.py
@@ -70,9 +70,9 @@ def upload():
                     Block.append(str(line.rstrip()))
                     
                 page = DP.convert_hex_file(Block)
-                log = AVR.AVR_ISP(TCP_IP,TCP_PORT,page)
+                # log = AVR.AVR_ISP(TCP_IP,TCP_PORT,page)
                 
-                return log
+                return page
         except:
             return 'Not allowed'
         
