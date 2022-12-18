9b50a99b09aeae180bdb582b492ccdfbc509ea53
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2022-12-19 00:17:41 +0700

    29

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ class MyWorker():
         messages.append({'title': 'Execution time:', 'content' : time.time() - start_time})
         with open('log.txt','w') as f:
             f.write(str(time.time()))
-            f.write("".join(log))
+            # f.write("".join(log))
             f.write(str(time.time()))
         return send_file('log.txt', as_attachment=True)
         # return redirect(url_for("upload"))
