08fac99df97cfd3d0281be9cfa29192a67fe4d79
Albert245 <nhat.phamc1929@hcmut.edu.vn>
2022-12-19 21:51:51 +0700

    29

--- main.py
+++ main.py
@@ -92,8 +92,8 @@ def upload():
                     
                 page = DP.convert_hex_file(Block)
                 # MyWorker(page)
-            # return render_template('about.html')
-            return 'wait'
+            return render_template('about.html')
+            # return 'wait'
         except:
             return 'Not allowed'
         
